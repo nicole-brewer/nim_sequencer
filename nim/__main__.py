d22dd17d1bbddab07e1e620ed37538e21fc65b91
Nicole Brewer <brewer36@purdue.edu>
2018-06-18 10:32:55 -0400

    Added clean-pyc to makefile

---
+++ nim/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""nim.__main__: executed when nim directory is called as script."""
+
+
+from .nim import main
+main()
---
+++ nim/src/argparse.py
@@ -0,0 +1,4 @@
+import argeparse
+
+single = argparse.ArgumentParser(description='Find the periodicity corresponding to a four variable subtraction set.')
+single.add_argument('
